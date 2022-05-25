be030f3647b4efae6734513e83f90bbc5616fd3c
duverney <duverney.loaiza@gmail.com>
2022-05-24 20:41:08 -0400

    rutas y para que sirven

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Cuando un usuario ingresa al sitio ppal lo que quiere que se vaya a la vista welcome
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "Bienvenido a la página ppal";
 });
+
+Route::get('cursos', function (){
+    return "Bienvenido a la página cursos";
+});
+
+Route::get('cursos/create', function (){
+    return "En esta pagina podras crear un curso";
+});
+
+/*Route::get('cursos/{curso}', function ($curso){
+    return "Bienvenido al curso $curso";
+});*/
+
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null){
+    if ($categoria){
+        return "Bienvenido al curso $curso de la categoria $categoria";
+    }else{
+        return "Bienvenido al curso $curso";
+    }
+
+});
+
+
+
